c0b12f83bc4dc279139dc0c9c778736255ea4678
alexandrsegedin <segedin@begroupcompanies.com>
2021-09-21 15:26:47 +0800

    [feedback_form][WIR] Заполняем страницу вакансии и создаём форму обратной связи

---
+++ src/feedback.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+    if (isset($_POST['email'])) {$email = $_POST['email'];}
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    $to = "segedin_alexandr@mail.ru"; /*Укажите ваш адрес электронной почты*/
+    $headers = "Content-type: text/plain; charset = utf-8";
+    $subject = "$formData";
+    $message = "$formData\n\nПочта: $email \n\nИмя: $name \n\nТелефон: $phone";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+        echo "<center>Спасибо за отправку вашего сообщения! Наши специалисты свяжутся с вами в течение часа.</center>";
+    }
+    else
+    {
+        echo "<center><b>Ошибка. Сообщение не отправлено! Проверьте правильность введенных данных</b></center>";
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
